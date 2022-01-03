93156b375efeaec3b9f2f0fd0cae7cdd5245ba2c
Zar Ni Tun <zarnitunkt@gmail.com>
2022-01-03 13:30:49 +0630

    Update error and fix

--- DebuggingExercises/DebugSix2.java
+++ DebuggingExercises/DebugSix2.java
@@ -1,7 +1,7 @@
 // DebugSix2.java
 // Display every character between Unicode 65 and 122
 // Start new line after 20 characters
-public class DebugSix2
+public class FixDebugSix2
 {
    public static void main(String args[])
    {
@@ -14,7 +14,7 @@ public class DebugSix2
       for(a = MIN; a <= MAX; a++)
         letter = (char)a;
         System.out.print("  " + letter);
-        if((a == STOPLINE1) & (a == STOPLINE2))
+        if((a == STOPLINE1) || (a == STOPLINE2))
           System.out.println();
       System.out.println("\nEnd of application")
     }
